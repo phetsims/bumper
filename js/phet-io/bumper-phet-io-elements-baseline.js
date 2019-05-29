9783fbf4db09f01d58dc3ad2a88dd253f9f0669c
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:32:02 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/bumper-phet-io-elements-baseline.js
+++ js/phet-io/bumper-phet-io-elements-baseline.js
@@ -961,7 +961,7 @@ window.phet.phetio.phetioElementsBaseline =
     "bumper.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
