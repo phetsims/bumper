21f0462eebd0e2d371daa09578448528b3eb99c4
zepumph <michael.kauzmann@colorado.edu>
2019-06-13 13:42:59 -0800

    update baselines and remove redundant GQ overrides, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/bumper-phet-io-elements-baseline.js
+++ js/phet-io/bumper-phet-io-elements-baseline.js
@@ -774,7 +774,7 @@ window.phet.phetio.phetioElementsBaseline =
     "bumper.navigationBar.phetButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
